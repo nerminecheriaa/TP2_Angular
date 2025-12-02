1750e22420be49435364785f5bb991a2b3fbb691
MelkiMeriem <meriem.melki@insat.ucar.tn>
2025-12-02 12:58:03 +0100

    Menu Done

--- ex3/src/app/app.routes.ts
+++ ex3/src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { CvComponent } from './components/cv/cv.component';
 import { AuthFormComponent } from './components/auth-form/auth-form.component';
 import { ImagesComponent } from './components/images/images.component';
 import { EmbauchesListComponent } from './components/embauches-list/embauches-list.component';
+import {CarouselComponent} from './components/carousel/carousel.component';
+
 export const routes: Routes = [
   { path: '', component: CvComponent },
   { path: 'auth', component: AuthFormComponent },
   { path: 'img', component: ImagesComponent },
-  {path: 'embauches', component: EmbauchesListComponent}
+  {path: 'embauches', component: EmbauchesListComponent},
+  { path: 'carousel', component: CarouselComponent },
+
+  { path: '**', redirectTo: 'cv' },
 
 ];
