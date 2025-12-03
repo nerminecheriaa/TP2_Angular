7260abf4b34b540be9e9c2c9f90c47a1724b625b
fatmatrabelsi17 <fatma.trabelsi@insat.ucar.tn>
2025-12-03 23:47:23 +0100

    delete cv fixed

--- ex3/src/app/services/cv.service.ts
+++ ex3/src/app/services/cv.service.ts
@@ -1,20 +1,20 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, catchError, map, tap } from 'rxjs';
 import { Cv } from '../models/cv';
 import { FakeCvService } from './fake-cv.service';
-import { mapCvApiToCv } from '../helpers/cv-mapper.helper';
 import { CV_API_CONFIG } from '../config/cv-config';
-import { CvApiResponse } from '../models/cv-api.model';
 import { ToasterService } from './toaster.service';
+import { catchError, of, tap } from 'rxjs';
+import { CvApiResponse } from '../models/cv-api.model';
+import { mapCvApiToCv } from '../helpers/cv-mapper.helper';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CvService {
   private readonly apiUrl = CV_API_CONFIG.url;
-  private readonly cvsSubject = new BehaviorSubject<Cv[]>([]);
-  public readonly cvs$ = this.cvsSubject.asObservable();
+
+  cvs = signal<Cv[]>([]); // signal pour stocker les CVs
 
   private http = inject(HttpClient);
   private fakeCvService = inject(FakeCvService);
@@ -24,41 +24,48 @@ export class CvService {
     this.loadCvs();
   }
 
-  private loadCvs(): void {
+  loadCvs(): void {
     this.http.get<CvApiResponse[]>(this.apiUrl).pipe(
-      map(apiList => apiList.map(api => mapCvApiToCv(api))),
-      tap(mappedCvs => this.cvsSubject.next(mappedCvs)),
-      catchError(error => {
-        this.toaster.showError('Problème API détecté. FakeCvs affichés.');
-        return this.fakeCvService.getFakeCvs();
+      tap(apiData => console.log('Données API brutes:', apiData)),
+      catchError(err => {
+        console.error('Erreur API CVs:', err);
+        this.toaster.showError('Problème API. Affichage des Fake CVs', 'Erreur');
+
+        this.fakeCvService.getFakeCvs().subscribe(fakeCvs => {
+          this.cvs.set(fakeCvs);
+        });
+
+        return of([]); // observable vide pour compléter le pipe
       })
-    ).subscribe(cvs => this.cvsSubject.next(cvs));
+    ).subscribe(apiCvs => {
+      if (apiCvs.length) {
+        const cvs = apiCvs.map(api => mapCvApiToCv(api)); 
+        this.cvs.set(cvs);
+        this.toaster.showSuccess('CVs chargés depuis l’API.');
+      }
+    });
   }
 
-
-  getCvs(): Observable<Cv[]> {
-    return this.cvs$;
+  getCvs() {
+    return of(this.cvs());
   }
 
-  getCvById(id: number): Observable<Cv | null> {
-    return this.cvs$.pipe(
-      map(list => list.find(cv => cv.id === id) ?? null)
-    );
+  getCvById(id: number) {
+    const cv = this.cvs().find(c => c.id === id) ?? null;
+    return of(cv);
   }
 
-  deleteCv(id: number): Observable<boolean> {
-    return this.http.delete<boolean>(`${this.apiUrl}/${id}`).pipe(
-      tap(success => {
-        if (success) {
-          const updated = this.cvsSubject.value.filter(cv => cv.id !== id);
-          this.cvsSubject.next(updated);
-        }
-      }),
-      catchError(() => this.fakeCvService.deleteFakeCv(id))
-    );
+  addCv(cv: Cv) {
+    const newCvs = [...this.cvs(), cv];
+    this.cvs.set(newCvs);
+    this.toaster.showSuccess('CV ajouté localement.');
+    return of(cv);
+  }
 
-
-}
-
-
+  deleteCv(id: number) {
+    const newCvs = this.cvs().filter(cv => cv.id !== id);
+    this.cvs.set(newCvs);
+    this.toaster.showSuccess('CV supprimé localement', 'Suppression');
+    return of(true); // Observable<boolean> pour compatibilité
+  }
 }
